f212268633936fe42d28a2777ee2f7c43c2dec83
Andy Ng <andy.hy.ng@gmail.com>
2018-11-14 18:06:17 -0500

    added grid functions, timer functions

--- js/app.js
+++ js/app.js
@@ -1,10 +1,16 @@
+
+const grid = {};
+grid.rows = 5;
+grid.answerGrid = [];
+grid.playerGrid = [];
+grid.tileClassArray = [];
+
+const timer = {};
+timer.initialTime = 20;
+
 $(function() {
 
-  let rows = 5;
-  let answerGrid = [];
-
-  // get an array of true/false with respect to # of squares in 
-  function randomGrid(rows) {
+  grid.randomGrid = function(rows) {
     filledGrid = []
     for (let currRow = 0; currRow < Math.pow(rows, 2); currRow++) {
       filledGrid.push(_.sample([true, false]));
@@ -12,7 +18,8 @@ $(function() {
     return filledGrid;
   }
 
-  function fillGrid(gridArray) {
+  grid.fillGrid = function(gridArray) {
+    gridArray = [];
     for (let currItem = 0; currItem < gridArray.length; currItem++) {
       if (gridArray[currItem] === true) {
         console.log($(".tile-" + (currItem + 1)));
@@ -21,9 +28,56 @@ $(function() {
     }
   }
 
-  answerGrid = randomGrid(rows);
-  fillGrid(answerGrid);
-  console.log(answerGrid);
+  grid.playerFillGrid = function() {
+    $('.tile').on('click', function() {
+      $(this).toggleClass("correct-tile");
+    })
+  } 
+
+  grid.playerGuessToArray = function() {
+    grid.playerGrid = [];
+    $('.grid-container > .tile').each(function () {
+      // iterate through each child in the .grid-container
+      if (this.hasClass("correct-tile")) {
+        // get all classes of the current element
+        grid.tileClassArray = $(this).attr("class").split(" ");
+        for (let tile = 0; tile < tileClassArray.length; tile++) {
+          if (grid.tileClassArray[tile].test(/tile-/)) {
+            grid["playerGrid"].push(grid.tileClassArray[tile]);
+          }
+        }
+      }
+      this.removeClass("correct-tile");
+    })
+  }
+
+  grid.clearGrid = function() {
+    $(".tile").removeClass("correct-tile");
+  }
+
+  timer.gameTimer = function() {
+    if (timer.initialTime < 0) {
+      console.log("timer ended");
+      clearInterval(timerFunction);
+      return true;
+      // quit game, display results
+    }
+    else {
+      $(".timer").html("0:" + timer.initialTime);
+      timer.initialTime--;
+      console.log(timer.initialTime);
+    }
+  }
+
+  
+
+  answerGrid = grid.randomGrid(grid.rows);
+  grid.fillGrid(answerGrid);
+  let timerFunction = setInterval(timer.gameTimer, 1000);
+
+  
+
+  // console.log(grid.answerGrid);
 
   // function obstacleMove(tileRow) {
   //   let speed = 200;
@@ -40,7 +94,7 @@ $(function() {
   //     // ** replace .box with .tile in future **
   //     let currPosition = $('.tile-x' + xPosition + tileRow);
 
-  //     setTimeout(() => drawObstacle(currPosition), (frame - 1) * (delayTime + speed));
+  //     setInterval(() => drawObstacle(currPosition), (frame - 1) * (delayTime + speed));
 
   //     frame++;
   //     xPosition++;
